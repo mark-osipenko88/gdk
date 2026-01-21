0d169d1759b3e69880ca2a3863afab0df30dd2b3
mark-osipenko88 <suaddmin@yandex.ru>
2026-01-22 01:11:28 +0700

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,17 @@
+import telebot
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Загружает .env файл
+# Создаем экземпляр бота
+bot = telebot.TeleBot(os.getenv('BOT_TOKEN'))
+# Функция, обрабатывающая команду /start
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+    bot.send_message(m.chat.id, 'Я на связи. Напиши мне что-нибудь )')
+# Получение сообщений от юзера
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    bot.send_message(message.chat.id, 'Вы написали: ' + message.text)
+# Запускаем бота
+bot.polling(none_stop=True, interval=0)
